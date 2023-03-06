552211b0fc80b284a1327a199518fa0b076b0f7a
Paul Tomlinson <paul@javascripting.uk>
2023-03-06 17:01:29 +0000

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,9 +12,11 @@ const IndexPage = () => (
       
       <p>Hello there! I'm Paul, a London-based web developer and I've been freelancing for the past 15 years.</p>
 
-      <p>I specialise in Headless Wordpress websites and Javascript applications (hence the domain name!). I've worked with a wide range of clients from large brands, such as Unilever, RedBull and the BBC to many smaller and medium sized companies. I also once worked with Bill Bailey and animated the inside of his head!</p>
+      <p>I specialise in Wordpress websites and Javascript applications (hence the domain name!). I've worked with a wide range of clients from large brands, such as Unilever, RedBull and the BBC to many smaller and medium sized companies. I also once worked with Bill Bailey and animated the inside of his head!</p>
 
-      <p>I'm also very interested in developing other opportunities for myself. I once had a luxury chocolate business - where my products were stocked in Selfridges and Harvey Nichols amongst others - and I'm now currently working on an exciting board game.</p>
+      <p>I pride myself on being a great communicator and a hard worker who creates pixel-perfect websites and always gets things done.</p>
+
+      <p>I'm also very interested in developing other opportunities for myself. I've had a luxury chocolate business, launched a board game and I'm now also pushing a new ecommerce brand.</p>
 
       <a className="red" href="mailto:paul@javascripting.uk">Hire me here</a>. I always aim to respond within 24 hours.
 
