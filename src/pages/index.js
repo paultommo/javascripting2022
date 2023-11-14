d1fc5cbc5c1a54fb9b50d92a4b06b8b141475c0c
Paul Tomlinson <paul@javascripting.uk>
2023-11-14 08:44:33 +0000

    marketing copy

--- src/pages/index.js
+++ src/pages/index.js
@@ -91,13 +91,13 @@ const IndexPage = ({
   return(
   <Layout>
 
-    <Seo title="Paul Tomlinson: Building websites that your customers will love ❤️" />
+    <Seo title="Paul Tomlinson: For Websites that Wow Marketers" />
 
      <div className="homepage-section one">
 
       <div className="intro">
       
-        <h1><span className="hello">Paul Tomlinson</span>Building websites that your<br/>customers will love ❤️</h1>
+        <h1><span className="hello">Paul Tomlinson</span>For websites that<br/>wow Marketers</h1>
         
         <h2>"Our client is thrilled with the end result. Thank you, Paul!"</h2>
 
@@ -109,7 +109,7 @@ const IndexPage = ({
 
         <div class="audit">
         
-        I'd be delighted to conduct a comprehensive review of your website and explore ways to enhance its appeal to your customers.
+        I'd be delighted to conduct a comprehensive review of your website and explore ways to make it wow for you and your customers.
 
         </div>
 
@@ -135,11 +135,19 @@ const IndexPage = ({
 
       <div className="copy">
 
-      And I can bring in the love by:
+      And I can wow marketers by:
 
       <ul>
+
+      <li><h2>Giving you a top quality service</h2>I will build you a high performing and seo-loving website.</li>
+      <li><h2>Taking care of all your worries</h2>I can look after all your design, copy, technology and project management needs and create you something great!</li>
+      <li><h2>Offering my experience</h2>I have 20 years experience in this industry, so I understand how to make high impact, user-friendly websites.</li>
+      <li><h2>Saving you time</h2>I’ll build you an intuitive CMS solution, which make website updates a breeze.</li>
+      <li><h2>Saving you money</h2>I’ll future-proof your website, so that you won’t need to pay for another in 12 months time.</li>
+      <li><h2>By being on the ball</h2>I always keep a close eye on the latest technology and trends in order to offer my clients the best solutions available.</li>
+      <li><h2>Giving you a marketing hand</h2>Having worked with so many marketers and previously run my own luxury chocolate business, I’m always here if you need a second opinion with your marketing!</li>
       
-      <li><h2>Helping you select the best solution for your project</h2>
+      {/* <li><h2>Helping you select the best solution for your project</h2>
       With extensive experience across various technologies and a diverse range of projects, I am well-equipped to assist you in selecting the ideal approach with confidence.
       </li>
 
@@ -161,7 +169,7 @@ const IndexPage = ({
 
       <li><h2>Addressing all the foundational aspects of web development</h2>
       I assure you that I will check off all the essential web development tasks, ranging from crafting a fully responsive website and SEO optimisation to implementing all the appropriate HTML tags.
-      </li>
+      </li> */}
 
       {/* <li><h2>Creating a website that is finely tuned for SEO optimization</h2>
       I will provide you with all the key ingredients to ensure Google loves your website!
@@ -171,9 +179,9 @@ const IndexPage = ({
       I also take pride in my strong interpersonal skills, whether interacting with clients, developers, or designers. I am committed to actively listening to your requirements to ensure that we collaboratively build a website that truly delights you and your customers!
       </li> */}
 
-      <li><h2>Assisting you to the fullest extent possible</h2>
+      {/* <li><h2>Assisting you to the fullest extent possible</h2>
       I collaborate with a diverse team of experts in design, SEO, and copywriting, ready to assist you with various aspects beyond just project development if needed.
-      </li>
+      </li> */}
 
 
       </ul>
